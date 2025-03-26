08aae75f06ba82ebfc71a027236c1ad8749b757e
als-menezes <andre.sousamenezes2@gmail.com>
2025-03-26 15:24:16 -0300

    > Configurações adicionais da migration

--- src/main/java/bootcamp/persistence/migration/MigrationStrategy.java
+++ src/main/java/bootcamp/persistence/migration/MigrationStrategy.java
@@ -1,8 +1,19 @@
 package bootcamp.persistence.migration;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.sql.Connection;
+import java.sql.SQLException;
 
+import liquibase.Liquibase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.AllArgsConstructor;
+
+import static bootcamp.persistence.config.ConnectionConfig.getConnection;
 
 @AllArgsConstructor
 public class MigrationStrategy {
@@ -10,6 +21,30 @@ public class MigrationStrategy {
     private final Connection connection;
 
     private void executeMigration() {
-        
+        var originalOut = System.out;
+        var originalError = System.err;
+        try {
+            try (var fos = new FileOutputStream("liquibase.log")) {
+                System.setOut(new PrintStream(fos));
+                System.setErr(new PrintStream(fos));
+                try (
+                        var connection = getConnection();
+                        var jdbcConnection = new JdbcConnection(connection);) {
+                    var liquibase = new Liquibase(
+                            "db/changeLog/db.changelog-master.yml",
+                            new ClassLoaderResourceAccessor(),
+                            jdbcConnection);
+                    liquibase.update();
+                } catch (SQLException | LiquibaseException e) {
+                    e.printStackTrace();
+                }
+                System.setErr(originalError);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            System.setOut(originalOut);
+            System.setErr(originalError);
+        }
     }
 }
